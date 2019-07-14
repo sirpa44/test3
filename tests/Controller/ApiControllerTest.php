a1ad0cc776bac63f6fee0cafd2c6550fa74f4878
sirpa44 <sirpa.antoine@gmail.com>
2019-07-14 12:43:02 +0200

    functionnal test

--- tests/Controller/ApiControllerTest.php
+++ tests/Controller/ApiControllerTest.php
@@ -1,22 +1,18 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: antoine
- * Date: 14.07.19
- * Time: 11:57
- */
+<?php declare(strict_types=1);
 
 namespace App\Tests\Controller;
 
-use App\Controller\ApiController;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ApiControllerTest extends WebTestCase
 {
 
-    public function testGetQuestion()
+    public function testGetQuestionsWithCsv()
     {
-        
+        $response = '[{"question":"question 1","createdAt":"2019-06-01 00:00:00","choices":{"choice 1":"response 1","choice 2":"response 2","choice 3":"response 3"}},{"question":"question 2","createdAt":"2019-06-01 00:00:01","choices":{"choice 1":"response 1","choice 2":"response 2","choice 3":"response 3"}}]';
+        $client = static::createClient();
+        $client->request('GET', '/questions/en');
+        $this->assertEquals($response, $client->getResponse()->getContent());
     }
 
 }
